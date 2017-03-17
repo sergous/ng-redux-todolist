0559f3e702c3e052689bfaf0a1a2b640fb008c67
Sergei Smirnov <Sergei_Smirnov2@epam.com>
2017-03-17 20:45:43 +0300

    update todo text input.

--- src/app/components/TodoTextInput.ts
+++ src/app/components/TodoTextInput.ts
@@ -1,4 +1,5 @@
 import {TodoService} from '../todos/todos';
+import {IScope} from 'angular';
 
 class TodoTextInputController {
   editing: boolean;
@@ -6,13 +7,34 @@ class TodoTextInputController {
   newTodo: boolean;
   onSave: Function;
 
+  static onUpdate(state: any) {
+    return {
+      text: state.text
+    };
+  }
+
   /** @ngInject */
-  constructor(public todoService: TodoService, public $window: any, public $timeout: any) {
+  constructor(
+    public todoService: TodoService,
+    public $window: any,
+    public $timeout: any,
+    $ngRedux: any,
+    $scope: IScope
+  ) {
     this.editing = this.editing || false;
     this.text = this.text || '';
     if (this.text.length) {
       this.focus();
     }
+
+    let disconnect = $ngRedux.connect(
+      state => TodoTextInputController.onUpdate(state)
+    )(this);
+
+    $scope.$on('$destroy', disconnect);
+
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleBlur() {

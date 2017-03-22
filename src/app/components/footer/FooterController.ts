f48d315a066b3f9054c38080267070763244d80d
Sergei Smirnov <Sergei_Smirnov2@epam.com>
2017-03-22 14:51:58 +0300

    add todos filter routing.

--- src/app/components/footer/FooterController.ts
+++ src/app/components/footer/FooterController.ts
@@ -3,6 +3,7 @@ import {SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE} from '../../constants/TodoFilters
 export default class FooterController {
   filters: string[];
   filterTitles: any;
+  filterStates: any;
 
   constructor() {
     this.filters = [SHOW_ALL, SHOW_ACTIVE, SHOW_COMPLETED];
@@ -10,6 +11,11 @@ export default class FooterController {
       [SHOW_ALL]: 'All',
       [SHOW_ACTIVE]: 'Active',
       [SHOW_COMPLETED]: 'Completed'
+    };
+    this.filterStates = {
+      [SHOW_ALL]: '.all',
+      [SHOW_ACTIVE]: '.active',
+      [SHOW_COMPLETED]: '.completed'
     };
   }
 }

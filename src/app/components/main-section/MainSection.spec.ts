b0f5f330a9b30022a48feab09fbfce4bb9bc3405
Sergei Smirnov <Sergei_Smirnov2@epam.com>
2017-03-24 20:46:07 +0300

    update main section spec.

--- src/app/components/main-section/MainSection.spec.ts
+++ src/app/components/main-section/MainSection.spec.ts
@@ -5,21 +5,11 @@ import MainSection from './';
 import rootReducer from '../../reducers/index';
 
 describe('MainSection component', () => {
-  class MockTodoService {
-    addTodo() {return; }
-    editTodo() {return; }
-    deleteTodo() {return; }
-    completeTodo() {return; }
-    completeAll() {return; }
-    clearCompleted() {return; }
-  }
-
   let component;
 
   beforeEach(() => {
     angular
       .module('mainSection', [ngRedux, 'app/components/main-section/MainSection.html'])
-      .service('todoService', MockTodoService)
       .component('mainSection', MainSection)
       .config(($ngReduxProvider) => {
         $ngReduxProvider.createStoreWith(
@@ -34,44 +24,15 @@ describe('MainSection component', () => {
   }));
 
   it('shoud call clearCompleted', () => {
-    spyOn(component.todoService, 'clearCompleted').and.callThrough();
-    component.handleClearCompleted();
-    expect(component.todoService.clearCompleted).toHaveBeenCalled();
+    spyOn(component, 'onUpdate').and.callThrough();
+    component.clearCompleted();
+    expect(component.onUpdate).toHaveBeenCalled();
   });
 
   it('shoud call completeAll', () => {
-    spyOn(component.todoService, 'completeAll').and.callThrough();
-    component.handleCompleteAll();
-    expect(component.todoService.completeAll).toHaveBeenCalled();
+    spyOn(component, 'onUpdate').and.callThrough();
+    component.completeAll();
+    expect(component.onUpdate).toHaveBeenCalled();
   });
 
-  it('shoud set selectedFilter', () => {
-    component.handleShow('show_completed');
-    expect(component.selectedFilter.type).toEqual('show_completed');
-    expect(component.selectedFilter.filter({completed: true})).toEqual(true);
-  });
-
-  it('shoud call completeTodo', () => {
-    spyOn(component.todoService, 'completeTodo').and.callThrough();
-    component.handleChange();
-    expect(component.todoService.completeTodo).toHaveBeenCalled();
-  });
-
-  it('shoud call deleteTodo', () => {
-    spyOn(component.todoService, 'deleteTodo').and.callThrough();
-    component.handleSave({text: ''});
-    expect(component.todoService.deleteTodo).toHaveBeenCalled();
-  });
-
-  it('shoud call editTodo', () => {
-    spyOn(component.todoService, 'editTodo').and.callThrough();
-    component.handleSave({text: 'Hello'});
-    expect(component.todoService.editTodo).toHaveBeenCalled();
-  });
-
-  it('shoud call deleteTodo', () => {
-    spyOn(component.todoService, 'deleteTodo').and.callThrough();
-    component.handleDestroy();
-    expect(component.todoService.deleteTodo).toHaveBeenCalled();
-  });
 });

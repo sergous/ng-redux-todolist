06b04e49061e0fa4cb89ffdff2faf4f71642fb6d
Sergei Smirnov <Sergei_Smirnov2@epam.com>
2017-03-24 15:41:46 +0300

    update header spec.

--- src/app/components/header/Header.spec.ts
+++ src/app/components/header/Header.spec.ts
@@ -5,13 +5,13 @@ import Header from './';
 import {Todo} from '../../todos/todos';
 import HeaderController from './HeaderController';
 import rootReducer from '../../reducers/index';
+import {List} from 'immutable';
+import {addTodo, deleteAll, deleteTodo} from '../../actions/index';
 
 describe('Header component', () => {
   let todos: Todo[];
   let defaultState;
   let store;
-  let targetObj;
-  let connect;
 
   beforeEach(() => {
     todos = [
@@ -49,10 +49,33 @@ describe('Header component', () => {
 
   it('should get the todos binded to the component', angular.mock.inject(($rootScope: angular.IRootScopeService, $compile: angular.ICompileService, $componentController) => {
     const component = $componentController('headerComponent', {}, {todos});
-    spyOn(component, 'handleSave').and.callThrough();
     expect(component.todos.length).toEqual(1);
+  }));
+
+  it('should remove todo to the component', angular.mock.inject(($rootScope: angular.IRootScopeService, $compile: angular.ICompileService, $componentController) => {
+    const component = $componentController('headerComponent', {}, {todos});
+    expect(component.todos.length).toEqual(1);
+    component.dispatcher(deleteTodo(0));
+    expect(component.todos.length).toEqual(0);
+  }));
+
+  it('should add todo to the component', angular.mock.inject(($rootScope: angular.IRootScopeService, $compile: angular.ICompileService, $componentController) => {
+    const component = $componentController('headerComponent', {}, {todos});
+    expect(component.todos.length).toEqual(1);
+    component.dispatcher(addTodo('New Task'));
+    expect(component.todos.length).toEqual(2);
+  }));
+
+  it('should call onUpdate on add new todo to the component', angular.mock.inject(($rootScope: angular.IRootScopeService, $compile: angular.ICompileService, $componentController) => {
+    const component = $componentController('headerComponent', {}, {todos});
+    component.dispatcher(deleteAll());
+    component.dispatcher(addTodo(todos[0].text));
+    const newState = List(todos).push({text: 'New Task', id: 1, completed: false}).toArray();
+    expect(component.todos.length).toEqual(1);
+    spyOn(component, 'handleSave').and.callThrough();
+    spyOn(component, 'onUpdate').and.callThrough();
     component.handleSave('New Task');
     expect(component.handleSave).toHaveBeenCalledWith('New Task');
-    expect(component.todos.length).toEqual(2);
+    expect(component.onUpdate).toHaveBeenCalledWith({todos: newState.reverse()});
   }));
 });

e35790045f94846727d85e603c991b7e54cb79dd
Sergei Smirnov <Sergei_Smirnov2@epam.com>
2017-03-29 20:33:11 +0300

    update todos count.

--- src/app/components/todos-count/index.ts
+++ src/app/components/todos-count/index.ts
@@ -1,11 +1,9 @@
 import * as angular from 'angular';
-import TodosCountController from './TodosCountController';
 
 const TodosCount: angular.IComponentOptions = {
   template: require('./TodosCount.html'),
-  controller: TodosCountController,
   bindings: {
-    todos: '<'
+    activeCount: '<'
   }
 };
 export default TodosCount;

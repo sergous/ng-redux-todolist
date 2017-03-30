a7f5737107ca4aed8e63e6770f069e055d96ae1e
Sergei Smirnov <Sergei_Smirnov2@epam.com>
2017-03-30 20:00:43 +0300

    update todo item spec.

--- src/app/components/todo-item/TodoItem.spec.ts
+++ src/app/components/todo-item/TodoItem.spec.ts
@@ -1,5 +1,8 @@
 import * as angular from 'angular';
 import 'angular-mocks';
+import 'angular-animate';
+import 'angular-aria';
+import 'angular-material';
 import TodoItem from './';
 
 const todo = {
@@ -8,19 +11,99 @@ const todo = {
         id: 0
       };
 
-describe('TodoItem component', () => {
+describe('TodoItem', () => {
+  let $scope: angular.IRootScopeService;
+  let checkbox: HTMLElement;
+  let editButton: HTMLElement;
+  let deleteButton: HTMLElement;
+  let element: JQuery;
+
+  const onCompleteSpy = jasmine.createSpy('onCompleteSpy');
+  const onSaveSpy = jasmine.createSpy('onSaveSpy');
+  const onDeleteSpy = jasmine.createSpy('onDeleteSpy');
+  const bindings = {
+    onComplete: onCompleteSpy,
+    onSave: onSaveSpy,
+    onDelete: onDeleteSpy,
+    editing: false,
+    todo
+  };
+
   beforeEach(() => {
     angular
-      .module('todoItem', ['app/components/todo-item/TodoItem.html'])
+      .module('todoItem', ['ngMaterial', 'app/components/todo-item/TodoItem.html'])
       .component('todoItem', TodoItem);
     angular.mock.module('todoItem');
   });
+  describe('element', () => {
+    beforeEach(angular.mock.inject((
+      $rootScope: angular.IRootScopeService,
+      $compile: angular.ICompileService
+    ) => {
+      $scope = Object.assign($rootScope.$new(), bindings);
+      element = $compile(`<todo-item todo="todo"
+                                     editing="editing"
+                                     on-save="onSave()"
+                                     on-delete="onDelete()"
+                                     on-complete="onComplete()"
+                          ></todo-item>`)($scope);
+      checkbox = element.find('md-checkbox')[0];
+      editButton = element.find('button')[0];
+      deleteButton = element.find('button')[1];
+    }));
 
-  it('should render correctly', angular.mock.inject(($rootScope: angular.IRootScopeService, $compile: angular.ICompileService) => {
-    const $scope = $rootScope.$new();
-    const element = $compile('<todo-item></todo-item>')($scope);
-    $scope.$digest();
-    const li = element.find('li');
-    expect(li).not.toBeNull();
-  }));
+    it('should render correctly', () => {
+      $scope.$digest();
+      expect(element.find('h3').length).toBe(1);
+      expect(element.find('h3')[0].textContent).toContain('Hello');
+      expect(element.find('todo-text-input').length).toBe(0);
+      expect(checkbox.getAttribute('aria-label')).toContain('Complete');
+      expect(editButton.getAttribute('aria-label')).toBe('Edit');
+      expect(deleteButton.getAttribute('aria-label')).toBe('Delete');
+    });
+
+    describe('edit button click', () => {
+      it('should toggle editing', () => {
+        editButton.click();
+        $scope.$digest();
+        expect($scope.editing).toBeTruthy();
+      });
+      it('should show input', () => {
+        $scope.editing = true;
+        $scope.$digest();
+        expect(element.find('h3').length).toBe(0);
+        expect(element.find('todo-text-input').length).toBe(1);
+      });
+      it('should hide input', () => {
+        $scope.editing = false;
+        $scope.$digest();
+        expect(element.find('h3').length).toBe(1);
+        expect(element.find('todo-text-input').length).toBe(0);
+      });
+    });
+
+    describe('complete checkbox', () => {
+      it('should render not completed todo', () => {
+        $scope.todo = {...todo, completed: false};
+        $scope.$digest();
+        expect(checkbox.getAttribute('aria-checked')).toBe('false');
+      });
+      it('should render completed todo', () => {
+        $scope.todo = {...todo, completed: true};
+        $scope.$digest();
+        expect(checkbox.getAttribute('aria-checked')).toBe('true');
+      });
+      it('should call onComplete', () => {
+        checkbox.click();
+        expect(onCompleteSpy).toHaveBeenCalled();
+      });
+    });
+
+    describe('delete button', () => {
+      it('should call onDelete', () => {
+        deleteButton.click();
+        expect(onDeleteSpy).toHaveBeenCalled();
+      });
+    });
+  });
 });

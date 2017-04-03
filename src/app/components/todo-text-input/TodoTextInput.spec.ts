dd877682eeae436b277ef7a39b030db60c72d103
Sergei Smirnov <Sergei_Smirnov2@epam.com>
2017-04-03 13:56:46 +0300

    split todo text input spec.

--- src/app/components/todo-text-input/TodoTextInput.spec.ts
+++ src/app/components/todo-text-input/TodoTextInput.spec.ts
@@ -2,7 +2,17 @@ import * as angular from 'angular';
 import 'angular-mocks';
 import TodoTextInput from './';
 
-describe('TodoTextInput', () => {
+describe('TodoTextInput component', () => {
+  let $scope: angular.IScope;
+  let element: JQuery;
+  let textInput: JQuery;
+  const onSaveSpy = jasmine.createSpy('onSaveSpy');
+  const bindings = {
+    text: 'New todo',
+    editing: true,
+    placeholder: 'What needs to be done?'
+  };
+
   beforeEach(() => {
     angular
       .module('todoTextInput', ['app/components/todo-text-input/TodoTextInput.html'])
@@ -10,89 +20,26 @@ describe('TodoTextInput', () => {
     angular.mock.module('todoTextInput');
   });
 
-  describe('input', () => {
-    let $scope: angular.IScope;
-    let element: JQuery;
-    let textInput: JQuery;
-    const onSaveSpy = jasmine.createSpy('onSaveSpy');
-    const bindings = {
-      text: 'New todo',
-      editing: true,
-      onSave: onSaveSpy,
-      placeholder: 'What needs to be done?'
-    };
+  beforeEach(angular.mock.inject((
+    $rootScope: angular.IRootScopeService,
+    $compile: angular.ICompileService
+  ) => {
+    $scope = Object.assign($rootScope.$new(), bindings);
+    element = $compile(`<todo-text-input
+                          text="text"
+                          placeholder="{{placeholder}}"
+                          editing="editing"
+                          on-save="onSave"
+                        ></todo-text-input>`)($scope);
+    $scope.$digest();
+    textInput = element.find('input');
+  }));
 
-    beforeEach(angular.mock.inject((
-      $rootScope: angular.IRootScopeService,
-      $compile: angular.ICompileService
-    ) => {
-      $scope = Object.assign($rootScope.$new(), bindings);
-      element = $compile(`<todo-text-input
-                            text="text"
-                            placeholder="{{placeholder}}"
-                            editing="editing"
-                            on-save="onSave"
-                          ></todo-text-input>`)($scope);
-      $scope.$digest();
-      textInput = element.find('input');
-    }));
-
-    it('should render correctly', () => {
-      expect(textInput.attr('type')).toEqual('text');
-      expect(textInput.attr('placeholder')).toEqual(bindings.placeholder);
-      expect(textInput.hasClass('edit')).toBeTruthy();
-      expect(textInput.hasClass('new-todo')).toBeFalsy();
-      expect(textInput.val()).toBe(bindings.text);
-    });
-  });
-
-  describe('component', () => {
-    it('should bind the text to the element', angular.mock.inject($componentController => {
-      const bindings = {
-        text: 'Hello'
-      };
-      const component = $componentController('todoTextInput', {}, bindings);
-      expect(component.text).toEqual('Hello');
-    }));
-
-    describe('onSave', () => {
-      let onSaveSpy;
-      beforeEach(() => {
-        onSaveSpy = jasmine.createSpy('onSaveSpy');
-      });
-
-      it('should call onSave', angular.mock.inject($componentController => {
-        const bindings = {
-          onSave: onSaveSpy,
-          text: 'Hello'
-        };
-        const component = $componentController('todoTextInput', {}, bindings);
-        component.handleBlur();
-        expect(component.onSave).toHaveBeenCalled();
-      }));
-
-      it('should not call onSave', angular.mock.inject($componentController => {
-        const bindings = {
-          onSave: onSaveSpy,
-          text: ''
-        };
-        const component = $componentController('todoTextInput', {}, bindings);
-        expect(component.text).toBe(bindings.text);
-        component.handleBlur();
-        expect(onSaveSpy).not.toHaveBeenCalled();
-      }));
-
-      it('should call onSave and clear text', angular.mock.inject($componentController => {
-        const bindings = {
-          onSave: onSaveSpy,
-          newTodo: true,
-          text: 'Hello'
-        };
-        const component = $componentController('todoTextInput', {}, bindings);
-        component.handleSubmit({keyCode: 13});
-        expect(component.onSave).toHaveBeenCalled();
-        expect(component.text).toEqual('');
-      }));
-    });
+  it('should render correctly editing', () => {
+    expect(textInput.attr('type')).toEqual('text');
+    expect(textInput.attr('placeholder')).toEqual(bindings.placeholder);
+    expect(textInput.hasClass('edit')).toBeTruthy();
+    expect(textInput.hasClass('new-todo')).toBeFalsy();
+    expect(textInput.val()).toBe(bindings.text);
   });
 });
---
+++ src/app/components/todo-text-input/TodoTextInputController.spec.ts
@@ -0,0 +1,62 @@
+import * as angular from 'angular';
+import 'angular-mocks';
+import TodoTextInput from './';
+
+describe('TodoTextInput', () => {
+  beforeEach(() => {
+    angular
+      .module('todoTextInput', ['app/components/todo-text-input/TodoTextInput.html'])
+      .component('todoTextInput', TodoTextInput);
+    angular.mock.module('todoTextInput');
+  });
+
+  describe('controller', () => {
+    it('should bind the text to the element', angular.mock.inject($componentController => {
+      const bindings = {
+        text: 'Hello'
+      };
+      const component = $componentController('todoTextInput', {}, bindings);
+      expect(component.text).toEqual('Hello');
+    }));
+
+    describe('onSave', () => {
+      let onSaveSpy;
+      beforeEach(() => {
+        onSaveSpy = jasmine.createSpy('onSaveSpy');
+      });
+
+      it('should call onSave', angular.mock.inject($componentController => {
+        const bindings = {
+          onSave: onSaveSpy,
+          text: 'Hello'
+        };
+        const component = $componentController('todoTextInput', {}, bindings);
+        component.handleBlur();
+        expect(component.onSave).toHaveBeenCalled();
+      }));
+
+      it('should not call onSave', angular.mock.inject($componentController => {
+        const bindings = {
+          onSave: onSaveSpy,
+          text: ''
+        };
+        const component = $componentController('todoTextInput', {}, bindings);
+        expect(component.text).toBe(bindings.text);
+        component.handleBlur();
+        expect(onSaveSpy).not.toHaveBeenCalled();
+      }));
+
+      it('should call onSave and clear text', angular.mock.inject($componentController => {
+        const bindings = {
+          onSave: onSaveSpy,
+          newTodo: true,
+          text: 'Hello'
+        };
+        const component = $componentController('todoTextInput', {}, bindings);
+        component.handleSubmit({keyCode: 13});
+        expect(component.onSave).toHaveBeenCalled();
+        expect(component.text).toEqual('');
+      }));
+    });
+  });
+});

0cdad99c9fbedbf5d536a6c8e0a03d95f5b740c2
Sergei Smirnov <Sergei_Smirnov2@epam.com>
2017-04-10 20:37:41 +0300

    add mock agent async delay.

--- src/app/agents/localStorage.agent.ts
+++ src/app/agents/localStorage.agent.ts
@@ -8,36 +8,53 @@ const tokenPlugin = req => {
   }
 };
 
+const asyncDelay = 500;
+
 const Auth = {
   current: () => {
     const user = JSON.parse(localStorage.getItem('mockUser'));
-    return new Promise((resolve) => resolve( {user} ));
+    return new Promise((resolve) => {
+      const result = () => resolve( {user} );
+      setTimeout(result, asyncDelay);
+    });
   },
   validate: (token: string) => {
     const user = JSON.parse(localStorage.getItem('mockUser'));
     const valid = user && user.token === token;
-    return new Promise((resolve, reject) =>
-      valid ? resolve( {user} ) : reject({errors: [new Error('Session not found')]}));
+    return new Promise((resolve, reject) => {
+      const result = () => valid ? resolve( {user} ) : reject({errors: [{message: 'Session not found'}]});
+      setTimeout(result, asyncDelay);
+    });
   },
   login: (email, password) => {
     const user = JSON.parse(localStorage.getItem('mockUser'));
     const valid = user && email === user.email && password === user.password;
-    return new Promise((resolve, reject) =>
-      valid ? resolve( {user} ) : reject({errors: [new Error('User not found')]}));
+    return new Promise((resolve, reject) => {
+      const result = () => valid ? resolve( {user} ) : reject({errors: [{message: 'User not found'}]});
+      return setTimeout(result, asyncDelay);
+    });
   },
   register: (username, email, password) => {
+    const existingUser = JSON.parse(localStorage.getItem('mockUser'));
     const user = {
       username,
       email,
       password,
       token: 'TOKENEXAMPLE'
     };
-    localStorage.setItem('mockUser', JSON.stringify( user ));
-    return new Promise((resolve) => resolve( {user} ));
+    const valid = user.email !== existingUser.email;
+    if (valid) localStorage.setItem('mockUser', JSON.stringify( user ));
+    return new Promise((resolve, reject) => {
+      const result = () => valid ? resolve( {user} ) : reject({errors: [{message: 'User already exist'}]});
+      setTimeout(result, asyncDelay);
+    });
   },
   save: user => {
     localStorage.setItem('mockUser', JSON.stringify( user ));
-    return new Promise((resolve) => resolve( {user} ));
+    return new Promise((resolve) => {
+      const result = () => resolve( {user} );
+      setTimeout(result, asyncDelay);
+    });
   }
 };
 

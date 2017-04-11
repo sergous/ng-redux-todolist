355532ffeffd0fc6d37d615f01a0e88dd4d62fe8
Sergei Smirnov <Sergei_Smirnov2@epam.com>
2017-04-11 18:40:19 +0300

    add app actions to index.

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,8 +1,10 @@
 import todoActions from './todo.actions';
 import listActions from './list.actions';
 import authActions from './auth.actions';
+import appActions from './app.actions';
 export default {
   ...todoActions,
   ...listActions,
-  ...authActions
+  ...authActions,
+  ...appActions
 };
